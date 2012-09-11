e0c73f07ee7f222440dcc6090c7912d68dee650c
Paul Oosthuysen <oosthuysen.paul@gmail.com>
2012-09-11 08:43:03 +0200

    - init env

--- Int/IntWebService.asmx
+++
@@ -1,1 +0,0 @@
-﻿<%@ WebService Language="C#" CodeBehind="IntWebService.asmx.cs" Class="Int.IntWebService" %>
--- Int/Properties/Settings.Designer.cs
+++ Int/Properties/Settings.Designer.cs
@@ -22,15 +22,5 @@ namespace Int.Properties {
                 return defaultInstance;
             }
         }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.WebServiceUrl)]
-        [global::System.Configuration.DefaultSettingValueAttribute("http://localhost:65272/IntWebService.asmx")]
-        public string Int_IntWebService_IntWebService {
-            get {
-                return ((string)(this["Int_IntWebService_IntWebService"]));
-            }
-        }
     }
 }
